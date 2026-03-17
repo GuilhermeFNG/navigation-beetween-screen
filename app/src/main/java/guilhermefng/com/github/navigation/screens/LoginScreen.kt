193fc3dc53ffa97f74cc251184a90fa0aefee15c
GuilhermeFNG <guilhermefng@hotmail.com>
2026-03-16 22:34:23 -0300

    Correção de erro no commit passado

--- app/src/main/java/guilhermefng/com/github/navigation/screens/LoginScreen.kt
+++ app/src/main/java/guilhermefng/com/github/navigation/screens/LoginScreen.kt
@@ -31,7 +31,7 @@ import androidx.navigation.NavHostController
                 color = Color.White
             )
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate("menu") },
                 colors = ButtonDefaults.buttonColors(Color.White),
                 modifier = Modifier.align(Alignment.Center)
             ) {
