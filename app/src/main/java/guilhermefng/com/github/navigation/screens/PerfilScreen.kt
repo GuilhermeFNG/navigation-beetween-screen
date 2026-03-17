2a47bc3cafd2241ecc5187408e850bff88dc25ad
GuilhermeFNG <guilhermefng@hotmail.com>
2026-03-16 22:37:10 -0300

    Passagem de parâmetros obrigatórios na tela de Perfil

--- app/src/main/java/guilhermefng/com/github/navigation/screens/PerfilScreen.kt
+++ app/src/main/java/guilhermefng/com/github/navigation/screens/PerfilScreen.kt
@@ -14,10 +14,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 
 @Composable
-fun PerfilScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+fun PerfilScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    nome: String
+) {
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -25,7 +29,7 @@ fun PerfilScreen(modifier: Modifier = Modifier, navController: NavHostController
             .padding(32.dp)
     ) {
         Text(
-            text = "PERFIL",
+            text = "PERFIL - $nome",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White
